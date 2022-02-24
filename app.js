146845de60227d202ade48993aec824154bcaf3e
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-24 22:54:09 +0530

    Inital Server Setup

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", (req, res) => {
+  res.render("home")
+});
+
+app.get("/login", (req, res) => {
+  res.render("login")
+});
+
+app.get("/register", (req, res) => {
+  res.render("register")
+});
+
+app.listen("3000", () => {
+  console.log("Server started on port 3000");
+})
