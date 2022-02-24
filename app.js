47c0f64bde8192d4f8b999befa7a7dafb0361421
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-25 02:01:17 +0530

    Hiding Credentials using dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
@@ -21,8 +22,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisasecretkey"
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// console.log(process.env.SECRET);
+
+// const secret = process.env.SECRET
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
