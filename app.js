4b10e72e21ef5ef50647fb9a5b81c17605f461e0
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-27 22:47:06 +0530

    Level 6 Security [ OAuth 2.0 Google Sign In ]

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate")
 
 const app = express();
 
@@ -35,9 +37,11 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // console.log(process.env.SECRET);
 
@@ -48,14 +52,58 @@ const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// Serialization & Deserialization for all Strategies
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Paste this code below every setting and above routes
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // Google + depriciation solution
+    // userProfileUrl: "http://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(cb);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // Home
 
 app.get("/", (req, res) => {
   res.render("home")
 });
+
+// OAuth Authentication
+// It brings up a popup to authenticate
+
+app.get("/auth/google",
+  passport.authenticate("google", {scope: ["profile"]})
+);
+
+// After Authentication
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", {failureRedirect: "/login"}),
+  (req, res) => {
+    res.redirect("/secrets");
+  }
+)
 
 // Register
 
